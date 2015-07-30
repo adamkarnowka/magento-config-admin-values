7551426ef510183a570e8674e64b1ab0f3145bd7
Adam Karnowka <adam.karnowka@creativestyle.pl>
2015-07-30 09:35:37 +0200

    Initial release

---
+++ app/code/local/Creativestyle/ConfigAdminValues/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adam
+ * Date: 29.07.15
+ * Time: 16:35
+ */ 
+class Creativestyle_ConfigAdminValues_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
